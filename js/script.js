e77802b1fe48f252cbc6845ab52b917f9e488457
Stephdev48 <147699498+Stephdev48@users.noreply.github.com>
2024-01-09 14:22:35 +0100

    màj 14h22 le 09 janv 2024

--- js/script.js
+++ js/script.js
@@ -1,1 +1,86 @@
-console.log("ok ça fonctionne !");
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => {return response.json()})
+        .then((data)=>{
+            console.log(data);
+            let fill1 = document.getElementById('para1');
+            fill1.innerHTML=data.value;
+        })
+
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => {return response.json()})
+        .then((data)=>{
+            console.log(data);
+            let fill2 = document.getElementById('para2');
+            fill2.innerHTML=data.value;
+        })
+
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => {return response.json()})
+        .then((data)=>{
+            console.log(data);
+            let fill3 = document.getElementById('para3');
+            fill3.innerHTML=data.value;
+        })
+
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => {return response.json()})
+        .then((data)=>{
+            console.log(data);
+            let fill4 = document.getElementById('para4');
+            fill4.innerHTML=data.value;
+        })
+
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => {return response.json()})
+        .then((data)=>{
+            console.log(data);
+            let fill5 = document.getElementById('para5');
+            fill5.innerHTML=data.value;
+        })
+
+
+// var btn = document.querySelector("button");
+// btn.addEventListener("click", main);
+
+function main(){
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => {return response.json()})
+        .then((data)=>{
+            console.log(data);
+            let fill1 = document.getElementById('para1');
+            fill1.innerHTML=data.value;
+        })
+
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => {return response.json()})
+        .then((data)=>{
+            console.log(data);
+            let fill2 = document.getElementById('para2');
+            fill2.innerHTML=data.value;
+        })
+
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => {return response.json()})
+        .then((data)=>{
+            console.log(data);
+            let fill3 = document.getElementById('para3');
+            fill3.innerHTML=data.value;
+        })
+
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => {return response.json()})
+        .then((data)=>{
+            console.log(data);
+            let fill4 = document.getElementById('para4');
+            fill4.innerHTML=data.value;
+        })
+
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => {return response.json()})
+        .then((data)=>{
+            console.log(data);
+            let fill5 = document.getElementById('para5');
+            fill5.innerHTML=data.value;
+        })
+}
+
